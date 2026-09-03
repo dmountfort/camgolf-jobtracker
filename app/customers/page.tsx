e1d78426bfbc6a677c5b4255c1c030ddabe4a23d
Darryn Mountfort <dev@khulasolutions.co.za>
2026-09-03 21:02:11 +0200

    Add customer and parts catalogues and preview reference editing

---
+++ app/customers/page.tsx
@@ -0,0 +1,2 @@
+import Catalog from "../components/catalog";
+export default function Customers(){return <Catalog kind="customers"/>}
---
+++ app/parts/page.tsx
@@ -0,0 +1,2 @@
+import Catalog from "../components/catalog";
+export default function Parts(){return <Catalog kind="parts"/>}
